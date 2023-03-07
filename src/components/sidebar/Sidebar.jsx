c6a25d97e643fd28fa75ba68f1cbcf8cbf37228d
MuhammadRadifa <m.difa123456@gmail.com>
2023-03-07 15:00:21 +0800

    resolve sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,24 +10,24 @@ const Sidebar = () => {
         <Link to={'/home'}>
           <h1 className='mb-11 hidden text-2xl font-bold md:block md:pl-5'>Kalkulator Kesehatan</h1>
         </Link>
-        <div className='flex items-center justify-evenly md:grid'>
+        <div className='flex w-full items-center justify-evenly md:grid  md:justify-center'>
           <Link to={'/home'}>
             <AiFillHome className='rounded-full bg-main p-1 text-[40px] text-white md:hidden' />
           </Link>
           <Link to={'/kalkulator-kalori'}>
-            <div className='group flex items-center gap-x-4 py-5 hover:bg-main'>
+            <div className='group flex items-center gap-x-4 py-5 hover:bg-main md:px-5'>
               <img src={Kalkulator} alt='Kalkulator Kalori' className='w-10' />
               <h2 className='hidden text-lg font-semibold group-hover:text-white md:block'>Kalkulator Kalori</h2>
             </div>
           </Link>
           <Link to={'/kalkulator-bmi'}>
-            <div className='group flex items-center gap-x-4 py-5 hover:bg-main'>
+            <div className='group flex items-center gap-x-4 py-5 hover:bg-main md:px-5'>
               <img src={BMI} alt='Kalkulator Kalori' className='w-10' />
               <h2 className='hidden text-lg font-semibold group-hover:text-white md:block'>Kalkulator BMI</h2>
             </div>
           </Link>
           <Link to={'/kalkulator-hidrasi'}>
-            <div className='group flex items-center gap-x-4 py-5 hover:bg-main'>
+            <div className='group flex items-center gap-x-4 py-5 hover:bg-main md:px-5'>
               <img src={Hidration} alt='Kalkulator Kalori' className='w-10' />
               <h2 className='hidden text-lg font-semibold group-hover:text-white md:block'>Kalkulator Hidrasi</h2>
             </div>
